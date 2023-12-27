3115bce1fba723d740a6fc12518ee3e98fd49333
khushi-vadera <148628587+khushi-vadera@users.noreply.github.com>
2023-12-27 14:22:45 +0530

    register

---
+++ register.php
@@ -0,0 +1,11 @@
+<?php
+
+echo"<table border=2 bordercolor=red>
+<tr><th>register</th></tr>
+<tr><th>name</th></tr>
+<tr><th>moblie no.</th></tr>
+<tr><th>city</th></tr>
+<tr><th>address</th></tr>"
+
+
+?>
